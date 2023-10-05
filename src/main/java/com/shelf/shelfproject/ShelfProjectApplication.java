322aaa1f1b29da7811ff9d33ad049273986d2df8
LorenaGullone <lorenagullone03@gmail.com>
2023-10-05 22:41:03 +0200

    Creazione Progetto

---
+++ src/main/java/com/shelf/shelfproject/ShelfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shelf.shelfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfProjectApplication.class, args);
+	}
+
+}
